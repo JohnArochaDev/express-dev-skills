e4d50d270ef8d50a09c7c0f2edec8107e57e7533
John Arocha Dev <jparocha777@gmail.com>
2023-12-11 23:21:59 -0500

    data down

---
+++ models/skill.js
@@ -0,0 +1,7 @@
+const skills =  [
+    {make: 'chevy', model: 'corvette', year: '1972', sexy: true},
+    {make: 'ford', model: 'mustang', year: '1966', sexy: true},
+    {make: 'mitsubishi' , model: 'eclipse', year: '1995', sexy: true},
+    {make: 'toyota', model: 'prius', year: 'any', sexy: false},
+    {make: 'datsun', model: '240z', year: '1972', sexy: true},
+]
