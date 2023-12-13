8b51643df1f48241f68332cd71389fcfa78f45e0
John Arocha Dev <jparocha777@gmail.com>
2023-12-12 21:54:37 -0500

    id's

--- models/car.js
+++ models/car.js
@@ -1,9 +1,9 @@
 const cars =  [
-    {make: 'chevy', name: 'corvette', year: '1972', sexy: true},
-    {make: 'ford', name: 'mustang', year: '1966', sexy: true},
-    {make: 'mitsubishi' , name: 'eclipse', year: '1995', sexy: true},
-    {make: 'toyota', name: 'prius', year: 'any', sexy: false},
-    {make: 'datsun', name: '240z', year: '1972', sexy: true},
+    {make: 'chevy', name: 'corvette', year: '1972', sexy: true, id:1234},
+    {make: 'ford', name: 'mustang', year: '1966', sexy: true, id:2345},
+    {make: 'mitsubishi' , name: 'eclipse', year: '1995', sexy: true, id:3456},
+    {make: 'toyota', name: 'prius', year: 'any', sexy: false, id:4567},
+    {make: 'datsun', name: '240z', year: '1972', sexy: true, id:5678},
 ]
 
 module.exports = {
