4155cdb2894e7797d13eb55e6550aefd9056edc9
John Arocha Dev <jparocha777@gmail.com>
2023-12-12 21:46:43 -0500

    fixed index

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var carCtrl = require('../controllers/cars')
+const Car = require('../models/car')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {title: 'Cars'})
+    res.render('index', {
+        title: 'Cars',
+        car: Car.getAll(),
+    
+    })
 });
 
-
-router.get('/', carCtrl.index);
+router.get('/:id', carCtrl.show);
 
 module.exports = router;
