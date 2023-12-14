04ddf2c4f322a0675949d2397e37142116a14451
John Arocha Dev <jparocha777@gmail.com>
2023-12-13 21:41:52 -0500

    need to req method-override

--- routes/cars.js
+++ routes/cars.js
@@ -4,6 +4,16 @@ var carCtrl = require('../controllers/cars')
 
 router.get('/', carCtrl.index);
 
+router.get('/new', carCtrl.new);
+
 router.get('/:id', carCtrl.show);
+
+router.get('/:id/edit', carCtrl.edit);
+
+router.post('/', carCtrl.create);
+
+router.delete('/:id', carCtrl.delete);
+
+router.put('/:id', carCtrl.update);
 
 module.exports = router;
